10d8d5b83961ff32f2992fd883447c8d9ba6277f
panshimin <panshimin111@163.com>
2025-05-15 19:18:35 +0800

    update 关闭 debug 模式；

--- server.py
+++ server.py
@@ -47,4 +47,4 @@ def analyze():
     return calculate_webp_memory_size(file.stream)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=False)
